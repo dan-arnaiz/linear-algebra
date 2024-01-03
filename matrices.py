397444504d38d1c7358d1c01539ab7fac1981847
Dan Arnaiz <124132003+dan-arnaiz@users.noreply.github.com>
2024-01-03 19:43:05 +0000

    Optimized coderuntime

--- matrices.py
+++ matrices.py
@@ -5,10 +5,9 @@ class Solution(object):
         :rtype: int
         """
         total_beams = 0
-        for i in range(len(bank)):
-            devices_i = [k for k in range(len(bank[i])) if bank[i][k] == '1']
-            for j in range(i + 1, len(bank)):
-                if all(bank[k].count('1') == 0 for k in range(i + 1, j)):
-                    devices_j = [k for k in range(len(bank[j])) if bank[j][k] == '1']
-                    total_beams += len(devices_i) * len(devices_j)
+        rows_with_devices = [i for i in range(len(bank)) if '1' in bank[i]]
+        for i in range(len(rows_with_devices) - 1):
+            devices_i = [k for k in range(len(bank[rows_with_devices[i]])) if bank[rows_with_devices[i]][k] == '1']
+            devices_j = [k for k in range(len(bank[rows_with_devices[i + 1]])) if bank[rows_with_devices[i + 1]][k] == '1']
+            total_beams += len(devices_i) * len(devices_j)
         return total_beams
