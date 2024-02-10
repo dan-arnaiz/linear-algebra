5dbafe636e0e6dd2284fe0b0d0aaa106568afe16
dan-arnaiz <dfarnaiz@gmail.com>
2024-02-10 22:41:22 +0800

    inversion

--- inversion/inversion.py
+++ inversion/inversion.py
@@ -12,7 +12,22 @@ def invert_matrix(matrix):
 matrix_2x2 = [[4, 7], [2, 6]]
 matrix_3x3 = [[4, 7, 2], [2, 6, 1], [1, 2, 3]]
 matrix_4x4 = [[4, 7, 2, 1], [2, 6, 1, 1], [1, 2, 3, 1], [1, 1, 1, 1]]
+matrixex1 = [[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]]
+test_5x5 = [[1, 3, -3, 3, -3], [2, 7, 0, -2, -4], [0, 0, 1, 0, 5], [0, 0, 2, 1, 2], [0, 0, 0, 3, -5]]
+
 
 print(invert_matrix(matrix_2x2))
 print(invert_matrix(matrix_3x3))
 print(invert_matrix(matrix_4x4))
+print(invert_matrix(matrixex1))
+print(invert_matrix(test_5x5))
+
+
+
+def scalar_multiply(matrix, scalars):
+    return [[row[i]*scalars[i] for i in range(len(row))] for row in matrix]
+
+matrix = [[4, 7, 2], [2, 6, 1], [1, 2, 3]]
+scalars = [-2, -3, 3]
+result = scalar_multiply(matrix, scalars)
+print(result)
