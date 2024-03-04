676ad99e24258aeca07c5a3d3a716d9f71990c91
Dan Arnaiz <124132003+dan-arnaiz@users.noreply.github.com>
2024-03-04 18:00:25 +0000

    dprs

---
+++ dprs/DentalPatientRecordSystem/src/main/java/com/mycompany/dentalpatientrecordsystem/DentalPatientRecordSystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dentalpatientrecordsystem;
+
+/**
+ *
+ * @author Marki Querequincia
+ */
+
+import javax.swing.*;
+public class DentalPatientRecordSystem {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        loginPopUp login = new loginPopUp();
+        login.displayForm();
+    }
+}
