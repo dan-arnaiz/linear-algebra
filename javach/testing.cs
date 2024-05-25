a4023c73caefd0077a70aa0869902f076c4d6a2e
dan-arnaiz <dfarnaiz@gmail.com>
2024-05-25 16:12:09 +0800

    nested loops

--- javach/testing.cs
+++ javach/testing.cs
@@ -1,15 +1,23 @@
 using System;
 
-class MainClass {
-
-    static public void Main()
+class Program
+{
+    static void Main()
     {
         Console.Write("Enter n: ");
         int n = Convert.ToInt32(Console.ReadLine());
 
-        for (int i = 1; i <= n; i += 2)
+        for (int i = 1; i <= n; i++)
         {
-            Console.Write(i);
+            for (int j = n; j > i; j--)
+            {
+                Console.Write(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++)
+            {
+                Console.Write("%");
+            }
+            Console.WriteLine();
         }
     }
 }
